9d235af9dfdacedb336a3dfbead196ed1a68de58
lovelesh24 <lovesinghsisodiya2403@gamil.com>
2024-02-25 23:19:10 +0530

    successfully completed the skill page

--- resumeproject/urls.py
+++ resumeproject/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home,name="home"),
     path('',include('serv.urls')),
-    path('',include('edu.urls'))
+    path('',include('edu.urls')),
+    path('contact/',views.contact,name="contact")
 ]
